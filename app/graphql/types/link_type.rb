508f9ae426fab68ceacae2befa18fd432d26559b
Bernie Chiu <bernie@shoplineapp.com>
2017-11-02 00:56:26 +0800

    GraphQL types and query for links

---
+++ app/graphql/types/link_type.rb
@@ -0,0 +1,7 @@
+Types::LinkType = GraphQL::ObjectType.define do
+  name 'Link'
+
+  field :id, !types.ID
+  field :url, !types.String
+  field :description, !types.String
+end
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,11 +3,9 @@ Types::QueryType = GraphQL::ObjectType.define do
   # Add root-level fields here.
   # They will be entry points for queries on your schema.
 
-  # TODO: remove me
-  field :testField, types.String do
-    description "An example field added by the generator"
-    resolve ->(obj, args, ctx) {
-      "Hello World!"
-    }
+  # Queries are just represented as fields
+  field :allLinks, !types[Types::LinkType] do
+    # Resolve would be called in order to fetch data for that field
+    resolve -> (obj, args, ctx) { Link.all }
   end
 end
