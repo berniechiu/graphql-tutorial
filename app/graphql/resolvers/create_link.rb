9059d8e1ff50904cf95e98f7167213bf9350e9e8
Bernie Chiu <bernie@shoplineapp.com>
2017-11-02 12:58:16 +0800

    Test create link mutation

---
+++ app/graphql/resolvers/create_link.rb
@@ -0,0 +1,19 @@
+class Resolvers::CreateLink < GraphQL::Function
+  # arguments passed as "args"
+  argument :description, !types.String
+  argument :url, !types.String
+
+  # return type from the mutation
+  type Types::LinkType
+
+  # the mutation method
+  # _obj - is parent object, which in this case is nil
+  # args - are the arguments passed
+  # _ctx - is the GraphQL context (which would be discussed later)
+  def call(_obj, args, _ctx)
+    Link.create!(
+      description: args[:description],
+      url: args[:url],
+    )
+  end
+end
