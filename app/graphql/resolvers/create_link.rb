23fd34013497c3f526cf92f98bb5d5479dc492c5
Bernie Chiu <bernie@shoplineapp.com>
2017-11-05 21:25:46 +0800

    Error handling with GraphQL

--- app/graphql/resolvers/create_link.rb
+++ app/graphql/resolvers/create_link.rb
@@ -16,5 +16,8 @@ class Resolvers::CreateLink < GraphQL::Function
       url: args[:url],
       user: ctx[:current_user]
     )
+  rescue ActiveRecord::RecordInvalid => e
+    # this would catch all validation errors and translate them to GraphQL::ExecutionError
+    GraphQL::ExecutionError.new("Invalid input: #{e.record.errors.full_messages.join(', ')}")
   end
 end
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,5 +1,7 @@
 class Link < ApplicationRecord
-  belongs_to :user
+  validates :url, presence: true, length: { minimum: 5 }
+  validates :description, presence: true, length: { minimum: 5 }
 
+  belongs_to :user
   has_many :votes
 end
