8d6c8f6ee233044e2569b106c8d175d7f6ac692d
Bernie Chiu <bernie@shoplineapp.com>
2017-11-05 20:07:05 +0800

    Create user types and mutation

--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -2,4 +2,5 @@ Types::MutationType = GraphQL::ObjectType.define do
   name "Mutation"
 
   field :createLink, function: Resolvers::CreateLink.new
+  field :createUser, function: Resolvers::CreateUser.new
 end
